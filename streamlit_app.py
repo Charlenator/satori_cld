8cdbbdd814a4f2993c5eb55644c5a429c2f039ce
Charlenator <8571895+Charlenator@users.noreply.github.com>
2024-09-19 14:28:53 +0000

    static files, config file, css in progress

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,15 @@ import streamlit as st
 from chatbot import ChatBot
 from config import TASK_SPECIFIC_INSTRUCTIONS
 
+
+def load_css():
+    with open("static/styles.css", "r") as f:
+        css = f"<style>{f.read()}</style>"
+        st.markdown(css, unsafe_allow_html=True)
+
 def main():
-   st.markdown(
-    """
+    st.markdown(
+        """
 <script src="https://cdn.tailwindcss.com"></script>
 <style>
 .chat-font {
@@ -12,64 +18,61 @@ def main():
 }
 </style>
     """,
-    unsafe_allow_html=True,
-)
+        unsafe_allow_html=True,
+    )
 
-   st.markdown(
+    st.markdown(
         '<h1 class="text-3xl font-bold mb-4">Ask me almost anything about Shipping!</h1>',
         unsafe_allow_html=True,
-   )
-   st.logo(
+    )
+    st.logo(
         image="https://shipmystuff-app.com/static/media/Logo_Optimised.02883188.png"
-   )
+    )
 
-   if "messages" not in st.session_state:
-       st.session_state.messages = [
-           {'role': "user", "content": TASK_SPECIFIC_INSTRUCTIONS},
-           {'role': "assistant", "content": "Understood"},
-       ]
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {'role': "user", "content": TASK_SPECIFIC_INSTRUCTIONS},
+            {'role': "assistant", "content": "Understood"},
+        ]
 
-   chatbot = ChatBot(st.session_state)
+    chatbot = ChatBot(st.session_state)
 
-   # Display user and assistant messages skipping the first two
-   for message in st.session_state.messages[2:]:
-       # ignore tool use blocks
-       if isinstance(message["content"], str):
-           with st.chat_message(message["role"]):
-               if message["role"] == "user":
-                   st.markdown(
-                    f'<div class="bg-blue-100 p-3 rounded-lg shadow-md max-w-xs ml-auto"> {message["content"]} </div>',
-                    unsafe_allow_html=True,
-                )
-               else:
-                # Assistant message styling
-                   st.markdown(
-                    f'<div class="bg-gray-100 p-3 rounded-lg shadow-md max-w-xs mr-auto"> {message["content"]} </div>',
-                    unsafe_allow_html=True
-                )
-                
-            
-               
+    # Display user and assistant messages skipping the first two
+    for message in st.session_state.messages[2:]:
+        # ignore tool use blocks
+        if isinstance(message["content"], str):
+            with st.chat_message(message["role"]):
+                if message["role"] == "user":
+                    st.markdown(
+                        f'<div class="chat-row"> {message["content"]} </div>',
+                        unsafe_allow_html=True,
+                    )
+                else:
+                 # Assistant message styling
+                    st.markdown(
+                        f'<div class="class="chat-row"> {message["content"]} </div>',
+                        unsafe_allow_html=True
+                    )
 
-   if user_msg := st.chat_input("Type your message here..."):
-       st.chat_message("user").markdown(user_msg)
+    if user_msg := st.chat_input("Type your message here..."):
+        st.chat_message("user").markdown(user_msg)
 
-       with st.chat_message("assistant"):
-           with st.spinner("Thinking..."):
-               response_placeholder = st.empty()
-               full_response = chatbot.process_user_input(user_msg)
-               st.markdown(
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response_placeholder = st.empty()
+                full_response = chatbot.process_user_input(user_msg)
+                st.markdown(
                     f'<p class="p-2 bg-gray-100 rounded-lg">{full_response}</p>',
                     unsafe_allow_html=True,
                 )
-    # Apply CSS styling
-   st.markdown(
+     # Apply CSS styling
+    st.markdown(
         """
 <script src="https://cdn.tailwindcss.com"></script>
         """,
         unsafe_allow_html=True,
     )
 
+
 if __name__ == "__main__":
-   main()
-   
+    main()
